e0a073255c19d01e6b5662ad38b89ae0729b504a
Paolo Bonini <45519144+PaoloBnn@users.noreply.github.com>
2019-06-12 20:36:03 +0200

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Retip library
+#'
+#' @docType data
+#'
+#' @usage data(retip_lib_v1)
+#'
+#' @keywords more than 16 different database with Retip Chemical descriptos calculated
+#'
+#' @references xxx
+#'
+#'
+#' @examples
+#' data(retip_lib_v1)
+
+"retip_lib_v1"
