8d2ec18e5ed03fa2b1bdb051d4ed0445eb8e647f
Neus Pou Amengual <neuspouamengual@gmail.com>
2024-06-04 10:59:26 +0200

    lib_v2 included in data.R file

--- R/data.R
+++ R/data.R
@@ -11,3 +11,8 @@
 #'
 #' @examples
 #' data(retip_lib_v2)
+
+retip_lib_v2 <- rbind(Retiplib::retip_lib_v2_1, Retiplib::retip_lib_v2_2)
+retip_lib_v2 <- rbind(retip_lib_v2, Retiplib::retip_lib_v2_3)
+
+"retip_lib_v2"
