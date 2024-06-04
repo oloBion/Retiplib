f7013524ca174ab87394ac0be33629d1e90d3a75
Neus Pou Amengual <neuspouamengual@gmail.com>
2024-06-04 11:58:52 +0200

    get.retiplib ftn (2) and roxygen2 changes

--- R/data.R
+++ R/data.R
@@ -2,16 +2,17 @@
 #' 
 #' @description Merge the three data objects to build the Retip library, which contains molecules from different databases with descriptors calculated.
 #'
-#' @export retip_lib
-#'
-#' @usage data(retip_lib_1)
+#' @export get.retiplib
 #'
 #' @return  The Retip library
 #'
 #' @examples
 #' get.retiplib()
 
-retip_lib <- rbind("retip_lib_1", "retip_lib_2")
-retip_lib <- rbind(retip_lib, "retip_lib_3")
+get.retiplib <- function() {
 
-retip_lib
+  retip_lib <- rbind(Retiplib::retip_lib_1, Retiplib::retip_lib_2)
+  retip_lib <- rbind(retip_lib, Retiplib::retip_lib_3)
+
+  return(retip_lib)
+}
