ac44370b6951cb85f69a8dbd3df878e8206c0397
Neus Pou Amengual <neuspouamengual@gmail.com>
2024-06-04 10:53:40 +0200

    R files per library part

---
+++ R/data_1.R
@@ -0,0 +1,15 @@
+#' Retip library
+#'
+#' @docType data
+#'
+#' @usage data(retip_lib_v2_1)
+#'
+#' @keywords more than 16 different database with Retip Chemical descriptos calculated
+#'
+#' @references xxx
+#'
+#'
+#' @examples
+#' data(retip_lib_v2_1)
+
+"retip_lib_v2_1"
---
+++ R/data_2.R
@@ -0,0 +1,15 @@
+#' Retip library
+#'
+#' @docType data
+#'
+#' @usage data(retip_lib_v2_2)
+#'
+#' @keywords more than 16 different database with Retip Chemical descriptos calculated
+#'
+#' @references xxx
+#'
+#'
+#' @examples
+#' data(retip_lib_v2_2)
+
+"retip_lib_v2_2"
